7b7fbb90cd2b6e43e50611431b432b3f692a5ce7
Amber Tsosie <atxambra@gmail.com>
2017-04-11 06:30:48 +0000

    Create and confirm requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,14 @@
 var request = require('request');
+var githubInfo = require('./private-key-config');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = githubInfo.user;
+var GITHUB_TOKEN = githubInfo.token;
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
   // ...
 }
 
