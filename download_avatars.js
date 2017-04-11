0ca59443c4a4512b153c47d49d8e52682e7ef348
Amber Tsosie <atxambra@gmail.com>
2017-04-11 17:49:49 +0000

    Add user agent and make request to fetch and log body

--- download_avatars.js
+++ download_avatars.js
@@ -7,11 +7,20 @@ var GITHUB_USER = githubInfo.user;
 var GITHUB_TOKEN = githubInfo.token;
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  // ...
+  console.log(requestURL); // just to confirm that it's a valid url
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request.get(options, cb);
 }
 
+// call function with parameters to test
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
