1b9bc2b70a951b6cbc58bd7b25995ffaf7abe0b5
Amber Tsosie <atxambra@gmail.com>
2017-04-11 19:40:25 +0000

    Implement downloadImageByUrl function

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,12 @@
-var request = require('request');
 var githubInfo = require('./private-key-config');
+var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = githubInfo.user;
 var GITHUB_TOKEN = githubInfo.token;
+
 
 function cb(err, response, body) {
   var bodyParsed = JSON.parse(body); // modified funtion to parse the body
@@ -13,10 +15,21 @@ function cb(err, response, body) {
   });
 }
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .pipe(fs.createWriteStream(filePath));
+}
+
+// call function with parameters to test
+downloadImageByURL("http://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
   var options = {
     url: requestURL,
     headers: {
